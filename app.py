f2b1e01fd27096c18b5995c43abbcc6254a14d41
Carolina Melo <caroolpmelo@gmail.com>
2020-06-01 05:15:07 -0300

    chore: :hammer: Adds heroku Procfile config and '/' route
    Signed-off-by: Carolina Melo <caroolpmelo@gmail.com>

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from resources.hotel import Hotels, Hotel
 app = Flask(__name__)
 api = Api(app)
 
-
-api.add_resource(Hotels, '/hotels')
+api.add_resource(Hotels, '/', '/hotels')
 api.add_resource(Hotel, '/hotels/<string:hotel_id>')
 
 
